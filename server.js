a6f1d07a8814020ab6a20199c3b528f011221fe4
omshankar2002 <shankarompal@gmail.com>
2024-06-26 00:41:55 -0700

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // request to body me store krlega
+const PORT = process.env.PORT || 3000;
+
 
 
 
@@ -21,7 +24,6 @@ app.use('/person',personRoutes);
 app.use('/menu',menuRoutes);
 
 
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('listening on port 3000')
 }) 
